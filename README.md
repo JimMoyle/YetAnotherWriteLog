312f108e302235434b21fa5067d276ea72dbc2f6
Jim Moyle <jmoyle@fslogix.com>
2018-01-29 23:23:11 +0000

    Quickly rewote and simplified

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # YetAnotherWriteLog
+
+	===========================================================================
+	 Created on:   	25/01/2017 09:35
+	 Created by:   	Jim Moyle
+	 Github: https://github.com/JimMoyle/YetAnotherWriteLog
+	 Twitter: @jimmoyle
+	===========================================================================
--- Write-Log.ps1
+++ Write-Log.ps1
@@ -1,31 +1,18 @@
-﻿<#
-	.NOTES
-	===========================================================================
-	 Created on:   	25/01/2017 09:35
-	 Created by:   	Jim Moyle
-	 Github: https://github.com/JimMoyle/Write-Log
-	 Twitter: @jimmoyle
-	===========================================================================
-	.DESCRIPTION
-		A description of the file.
-#>
-
-function Write-Log {
+﻿function Write-Log {
     [CmdletBinding(DefaultParametersetName = "LOG")]
     Param (
-        [Parameter(Mandatory = $false,
+        [Parameter(Mandatory = $true,
             ValueFromPipelineByPropertyName = $true,
             Position = 0,
             ParameterSetName = 'LOG')]
         [ValidateNotNullOrEmpty()]
-        [Alias("LogContent")]
         [string]$Message,
 
         [Parameter(Mandatory = $false,
             Position = 1,
             ParameterSetName = 'LOG')]
         [ValidateSet("Error", "Warn", "Info")]
-        [string]$Level,
+        [string]$Level = "Info",
 
         [Parameter(Mandatory = $false,
             Position = 2)]
@@ -39,21 +26,11 @@ function Write-Log {
         [switch]$StartNew,
 
         [Parameter(Mandatory = $false,
-            Position = 3,
-            ParameterSetName = 'LOG')]
-        [switch]$IncludeDebug,
-
-        [Parameter(Mandatory = $false,
-            Position = 4,
-            ValueFromPipelineByPropertyName = $true,
-            ParameterSetName = 'LOG')]
-        [object]$InvocationInfo,
-
-        [Parameter(Mandatory = $false,
             Position = 5,
+            ValueFromPipeline = $true,
             ValueFromPipelineByPropertyName = $true,
-            ParameterSetName = 'LOG')]
-        [object]$Exception
+            ParameterSetName = 'EXCEPTION')]
+        [System.Management.Automation.ErrorRecord]$Exception
 
     )
 
@@ -62,79 +39,60 @@ function Write-Log {
         $expandedParams = $null
         $PSBoundParameters.GetEnumerator() | ForEach-Object { $expandedParams += ' -' + $_.key + ' '; $expandedParams += $_.value }
         Write-Verbose "Starting: $($MyInvocation.MyCommand.Name)$expandedParams"
-        $outMessage = @()
     }
     PROCESS {
-        if ($Exception) {
-            $Message = $Exception.message
-            $Level = 'Error'
-        }
 
-        if ($null -eq $Level) {
-            $Level = 'Info'
-        }
-
-        if ($StartNew) {
-            Write-Verbose -Message "Deleting log file $Path if it exists"
-            Remove-Item $Path -Force -ErrorAction SilentlyContinue
-            $Message = 'Starting Log'
-            Write-Verbose -Message 'Deleted log file if it exists'
-        }
-
-        if (!(Test-Path $Path)) {
-            Write-Verbose "Creating $Path."
-            New-Item $Path -Force -ItemType File | Out-Null
-            Write-Verbose 'Created Log File'
-        }
-
-        Write-Verbose 'Getting Date for our Log File'
-        $FormattedDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-        Write-Verbose 'Date is $FormattedDate'
-
-        $normalOut = [PSCustomObject]@{
-            Message = $message; Level = $level
-        }
-
-        $outMessage += $normalOut
-
-        if ($IncludeDebug -and $InvocationInfo) {
-            $Debug = [PSCustomObject]@{
-                Message = $InvocationInfo.line.TrimEnd(); Level = 'Debug'
+        switch ($PSCmdlet.ParameterSetName) {
+            EXCEPTION {
+                Write-Log -Level Error -Message $Exception.Exception.Message
+                break
             }
-            $outMessage += $Debug
-        }
-
-        $outmessage | ForEach-Object {
-
-            # Write message to error, warning, or verbose pipeline and specify $LevelText
-            switch ($_.Level) {
-                'Error' {
-                    $LevelText = 'ERROR:  '; break
-                }
-                'Warn' {
-                    $LevelText = 'WARNING:'; break
-                }
-                'Info' {
-                    $LevelText = 'INFO:   '; break
-                }
-                'Debug' {
-                    $LevelText = 'DEBUG:  '; break
-                }
-                default {
-                    Write-Verbose 'Did not find a match in switch'
-                }
+            STARTNEW {
+                Write-Verbose -Message "Deleting log file $Path if it exists"
+                Remove-Item $Path -Force -ErrorAction SilentlyContinue
+                Write-Verbose -Message 'Deleted log file if it exists'
+                Write-Log 'Starting Logfile'
+                break
             }
+            LOG {
+                Write-Verbose 'Getting Date for our Log File'
+                $FormattedDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+                Write-Verbose 'Date is $FormattedDate'
 
-            $logmessage = "$FormattedDate $LevelText $($_.Message)"
-            Write-Verbose $logmessage
+                switch ( $Level ) {
+                    'Error' {
+                        $LevelText = 'ERROR:  '; break
+                    }
+                    'Warn' {
+                        $LevelText = 'WARNING:'; break
+                    }
+                    'Info' {
+                        $LevelText = 'INFO:   '; break
+                    }
+                    default {
+                        Write-Verbose 'Did not find a match in switch'
+                    }
+                }
 
-            # Write log entry to $Path
+                $logmessage = "$FormattedDate $LevelText $Message"
+                Write-Verbose $logmessage
 
-            $logmessage | Out-File -FilePath $Path -Append
+                # Write log entry to $Path
 
+                $logmessage | Add-Content -Path $Path
+            }
+            Default { Write-Error 'Did not resolve parameter set'}
         }
+
     }
     END {
         Write-Verbose "Finished: $($MyInvocation.Mycommand)"
     }
 } # enable logging
+
+Write-Log -StartNew
+Write-Log -Level Error 'Error test'
+Write-Log -Level Warn 'Warn test'
+$error.Clear()
+get-item c:\nope -ErrorAction SilentlyContinue
+$error[0] | Write-Log
