d066523f3ecf403a70b695e8b13d8b4cb1e8e93d
Jim Moyle <jmoyle@fslogix.com>
2018-02-01 18:09:39 +0000

    deleted file

--- Tests/Write-Log.tests.ps1
+++ Tests/Write-Log.tests.ps1
@@ -5,6 +5,13 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 
 Describe 'Write-Log' {
     It 'Should not throw when writing JSON files' {
+        Write-Log 'throw test' -path TestDrive:\json.log
+    }
+    It 'Should produce help' {
+        $h = help Write-Log
+        $h.count | Should BeGreaterThan 10
+    }
+    It 'Should not throw when writing JSON files' {
         Write-Log 'JSON test' -path TestDrive:\json.log -JSONFormat
     }
 }
